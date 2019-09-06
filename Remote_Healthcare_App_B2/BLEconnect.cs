5ad8c1cf5733e9213a105d82b0f4897f67938565
Luke Taylor <luketaylor001@gmail.com>
2019-09-06 17:05:20 +0200

    Heartrate wordt uitgelezen

--- Remote_Healthcare_App_B2/BLEconnect.cs
+++ Remote_Healthcare_App_B2/BLEconnect.cs
@@ -42,8 +42,8 @@ namespace ErgoConnect
 
             Thread.Sleep(1000);
 
-            ScanConnectForErgo(ergometerBLE, ergometerSerialLastFiveNumbers);
-            //ScanConnectForHR(heartrateBLE);
+            //ScanConnectForErgo(ergometerBLE, ergometerSerialLastFiveNumbers);
+            ScanConnectForHR(heartrateBLE);
         }
 
         public async Task ScanConnectForErgo(BLE ergometerBLE, System.String ergometerSerialLastFiveNumbers)
@@ -95,7 +95,7 @@ namespace ErgoConnect
             await heartrateSensorBLE.SetService("HeartRate");
 
             // Subscribe
-            heartrateSensorBLE.SubscriptionValueChanged += Ble_SubscriptionValueChanged;
+            heartrateSensorBLE.SubscriptionValueChanged += HR_SubscriptionValueChanged;
             await heartrateSensorBLE.SubscribeToCharacteristic("HeartRateMeasurement");
         }
 
@@ -134,6 +134,26 @@ namespace ErgoConnect
             ble.WriteCharacteristic("6e40fec1-b5a3-f393-e0a9-e50e24dcca9e", resistance);
         }
 
+        private void HR_SubscriptionValueChanged(object sender, BLESubscriptionValueChangedEventArgs e)
+        {
+            byte[] rawData = e.Data;
+            //int messageLength = rawData[1];
+            //byte[] message = rawData.Take(messageLength).ToArray();
+            //byte[] checksum = rawData.Skip(messageLength).ToArray();
+            //bool isCorrect = CheckXorValue(rawData, checksum);
+
+            int heartRate = rawData[1];
+
+            Console.WriteLine("HeartRate: " + heartRate);
+
+
+
+            //if (isCorrect)
+            //{
+
+            //}
+
+        }
         private void Ble_SubscriptionValueChanged(object sender, BLESubscriptionValueChangedEventArgs e)
         {
             // Attempt to change resistance of vehicle.
@@ -141,7 +161,17 @@ namespace ErgoConnect
             byte[] rawData = e.Data;
             int messageLength = rawData[1];
             byte[] message = rawData.Skip(4).Take(messageLength).ToArray();
-            int pageNumber = message[0];
+            int pageNumber = 0;
+
+            if (message.Length != 0)
+            {
+                pageNumber = message[0];
+            }
+            else
+            {
+                Console.WriteLine("Message is zero");
+            }
+            
             byte[] checksum = rawData.Skip(4).Skip(messageLength).ToArray();
             //Console.WriteLine((int)rawData[4]);
 
@@ -178,8 +208,8 @@ namespace ErgoConnect
                     double[] data = { updateEventCount, instanteousCadence, accumulatedPower, instanteousPower };
                     this.dataHandler.addBLEDataForDataPage25(data);
                 }
-                this.dataHandler.readLastData();
-                this.dataHandler.writeData();
+                //this.dataHandler.readLastData();
+                //this.dataHandler.writeData();
             }
         }
     }
