c26a9e03a9484b43c273e8b506a3974e9d212cc8
Luke Taylor <luketaylor001@gmail.com>
2019-09-23 16:30:55 +0200

    quick cleanup

--- Server/ServerClient.cs
+++ Server/ServerClient.cs
@@ -29,55 +29,9 @@ namespace Server
         private void OnRead(IAsyncResult ar)
         {
             Console.WriteLine("Received Data");
-            //check what type of package is received
-            //vr or bike or connect?
 
             int receivedBytes = stream.EndRead(ar);
             string message = Encoding.ASCII.GetString(buffer, 0, receivedBytes);
-
-            string[] data = Regex.Split(message, "#");
-            Console.WriteLine(data[0]);
-            handleMessage(data);
-            stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
-        }
-
-        private void handleMessage(string[] data)
-        {
-            
-            switch (data[0])
-            {
-                case "Connect":
-                    Console.WriteLine($"User: {data[1]}, has connected");
-                    patients.Add(data[1], new List<string[]>());
-                    break;
-                case "Datapackage":
-                    Console.WriteLine($"Data package recieved from: {data[1]}");
-                    HandleDataMessage(data);
-
-                    break;
-                default:
-                    Console.WriteLine($"Tag: {data[0]} not recognized!");
-                    stream.Flush(); // Added for debug purposes.
-                    break;
-            }
-        }
-
-        private void HandleDataMessage(string[] data)
-        {
-            switch (data[2])
-            {
-                case "BikeData16":
-                    patients[data[1]].Add(data);
-                    Console.WriteLine(patients[data[1]].ToString());
-                    break;
-                case "BikeData25":
-                    patients[data[1]].Add(data);
-                    Console.WriteLine(patients[data[1]].ToString());
-                    break;
-                default:
-                    Console.WriteLine("Error, type not recognized");
-                    break;
-            }
         }
     }
 }
