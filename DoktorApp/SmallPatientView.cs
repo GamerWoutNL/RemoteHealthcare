ddad946277cd5d21b5e28d2283c8acf3bf3ee634
Ties Tienhoven <tiestienhoven@gmail.com>
2019-10-15 14:33:15 +0200

    (dummy)data loading into chart created

--- DoktorApp/SmallPatientView.cs
+++ DoktorApp/SmallPatientView.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace DoktorApp
 {
     public partial class SmallPatientView : UserControl
     {
+
+        public string patientName { get; set; }
+        public string patientNumber { get; set; }
+        public int resistance { get; set; }
+        internal List<HeartrateDatapoint> HeartrateDatapoints { get => heartrateDatapoints; set => heartrateDatapoints = value; }
+
+        private List<HeartrateDatapoint> heartrateDatapoints = new List<HeartrateDatapoint>();
+        
+
         public SmallPatientView()
         {
             InitializeComponent();
@@ -22,6 +32,14 @@ namespace DoktorApp
             InitializeComponent();
             this.PatientNameLabel.Text = $"Patient naam: {PatientName}";
             this.PatientNumberLabel.Text = $"Patient nummer: {PatientNumber}";
+            
+            this.HeartrateChart.Series.Clear();
+
+            AddHeartrateDataPoint(new HeartrateDatapoint(DateTime.Now, 78));
+            AddHeartrateDataPoint(new HeartrateDatapoint(new DateTime(2017, 05, 17, 12, 12, 12), 89));
+            
+            
+            
         }
 
         private void PatientNameLabel_Click(object sender, EventArgs e)
@@ -29,5 +47,30 @@ namespace DoktorApp
             DetailedPatientView patientView = new DetailedPatientView();
             patientView.Show();
         }
+
+        private void AddHeartrateDataPoint(HeartrateDatapoint hrPoint)
+        {
+            this.heartrateDatapoints.Add(hrPoint);
+            this.heartrateDatapoints.OrderByDescending(heartrateDatapoint => heartrateDatapoint.timestamp);
+
+            this.HeartrateChart.Series.Clear();
+
+            Series heartrateSeries = new Series();
+            heartrateSeries.ChartType = SeriesChartType.Line;
+            heartrateSeries.Name = "Heartrate";
+            
+            int counter = 1;
+            foreach(HeartrateDatapoint hrPoint1 in HeartrateDatapoints)
+            {
+                
+                heartrateSeries.Points.Add(new DataPoint(counter, hrPoint1.heartrateData));
+                counter++;
+            }
+
+            this.HeartrateChart.Series.Add(heartrateSeries);
+        }
+
+        
+
     }
 }
---
+++ DoktorApp/SpeedDataPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoktorApp
+{
+    class SpeedDataPoint
+    {
+        public DateTime timestamp { get; set; }
+        public double speedData { get; set; }
+
+        public SpeedDataPoint(DateTime timestamp, double speedData)
+        {
+            this.timestamp = timestamp;
+            this.speedData = speedData;
+        }
+    }
+}
