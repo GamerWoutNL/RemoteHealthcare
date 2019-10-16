75c1f98585680370dd4d69859059a30c35b296c7
Ties <tiestienhoven@gmail.com>
2019-10-16 12:06:18 +0200

    Method to add data to chart made + small changes to charts in small view

--- DoktorApp/ChartUtils.cs
+++ DoktorApp/ChartUtils.cs
@@ -12,9 +12,23 @@ namespace DoktorApp
     class ChartUtils
     {
 
-        static void addDatapointToListForChart(CustomDatapoint datapoint,List<CustomDatapoint> customDatapoints, Chart Chart)
+        public static void addDatapointToListForChart(CustomDatapoint datapoint,List<CustomDatapoint> customDatapoints, Chart Chart)
         {
-            
+            customDatapoints.Add(datapoint);
+            customDatapoints.Sort((x, y) => y.timestamp.CompareTo(x.timestamp));
+            Chart.Series.Clear();
+
+            Series series1 = new Series();
+            series1.ChartType = SeriesChartType.Line;
+
+            int counter = 1;
+            foreach(CustomDatapoint datapoint1 in customDatapoints)
+            {
+                series1.Points.Add(new DataPoint(counter, datapoint1.data));
+                counter++;
+            }
+
+            Chart.Series.Add(series1);
         }
 
     }
