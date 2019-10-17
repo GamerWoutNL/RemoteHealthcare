e0f44695f475f33172ceb32c60683b22e303d639
Ties Tienhoven <tiestienhoven@gmail.com>
2019-10-17 10:37:01 +0200

    ergo login added again

---
+++ Remote_Healthcare_App_B2/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ErgoConnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NumberLabel = new System.Windows.Forms.Label();
+            this.NameField = new System.Windows.Forms.TextBox();
+            this.NumberField = new System.Windows.Forms.TextBox();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(60, 40);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(72, 13);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Patient naam:";
+            this.NameLabel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // NumberLabel
+            // 
+            this.NumberLabel.AutoSize = true;
+            this.NumberLabel.Location = new System.Drawing.Point(49, 67);
+            this.NumberLabel.Name = "NumberLabel";
+            this.NumberLabel.Size = new System.Drawing.Size(83, 13);
+            this.NumberLabel.TabIndex = 1;
+            this.NumberLabel.Text = "Patient nummer:";
+            // 
+            // NameField
+            // 
+            this.NameField.Location = new System.Drawing.Point(138, 37);
+            this.NameField.Name = "NameField";
+            this.NameField.Size = new System.Drawing.Size(100, 20);
+            this.NameField.TabIndex = 2;
+            // 
+            // NumberField
+            // 
+            this.NumberField.Location = new System.Drawing.Point(138, 64);
+            this.NumberField.Name = "NumberField";
+            this.NumberField.Size = new System.Drawing.Size(100, 20);
+            this.NumberField.TabIndex = 3;
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.Location = new System.Drawing.Point(52, 94);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(186, 23);
+            this.ConfirmButton.TabIndex = 4;
+            this.ConfirmButton.Text = "Confirm Data";
+            this.ConfirmButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 188);
+            this.Controls.Add(this.ConfirmButton);
+            this.Controls.Add(this.NumberField);
+            this.Controls.Add(this.NameField);
+            this.Controls.Add(this.NumberLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label NumberLabel;
+        private System.Windows.Forms.TextBox NameField;
+        private System.Windows.Forms.TextBox NumberField;
+        private System.Windows.Forms.Button ConfirmButton;
+    }
+}
