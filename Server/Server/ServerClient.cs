55c831421e31d58efee01c7eead994ced513ba08
Luke Taylor <luketaylor001@gmail.com>
2019-10-17 14:34:03 +0200

    Revert "Merge branch 'feature/FileWriter' into development"
    This reverts commit 3f987f8e66d23c0a5ef948c3f3aadc3cdcea34e9.

--- Server/Server/ServerClient.cs
+++ Server/Server/ServerClient.cs
@@ -6,156 +6,158 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Threading;
+using Server;
+using Server.Data;
 
 namespace Server
 {
-    public enum TagErgo
-    {
-		AC, //action
-
-			// Page 16
-		ET, // Elapsed time
-        DT, // Distance travelled
-        SP, // Speed
-        HR, // Heartrate
-
-        // Page 25
-        EC, // Event count
-        IC, // Instanteous cadence
-        AP, // Accumulated power
-        IP, // Instanteous power
-
-        // Extra
-        EOF, // End Of File
-		UN, //username
-		PW, //password
-        ID,  // Tag of Ergometer / simulator ID
-        TS,  // Timestamp
-        MT,   //The Message type of the message
-      
-    }
-
-    public enum TagDoctor
-    {
-
-    }
-
-    public enum TagVR
-    {
-
-    }
-
     class ServerClient
     {
-        private TcpClient tcpclient;
+        private TcpClient client;
 		private Server server;
         private NetworkStream stream;
         private byte[] buffer;
         private string totalBuffer;
+        private Dictionary<String, ClientData> clientDatas; // Should use ErgoID as a key.
+        private Dictionary<String, String> boundData; // patientNumber = key, ErgoID = value
+		public string ergoID { get; set; }
 
         public ServerClient(TcpClient client, Server server)
         {
-            this.tcpclient = client;
-            this.stream = client.GetStream();
+            this.client = client;
+            this.stream = this.client.GetStream();
 			this.server = server;
             this.buffer = new byte[1024];
-            this.totalBuffer = String.Empty;
-            this.stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+            this.totalBuffer = string.Empty;
+			this.ergoID = string.Empty;
+			this.clientDatas = new Dictionary<String, ClientData>();
+
+			this.stream.BeginRead(this.buffer, 0, this.buffer.Length, new AsyncCallback(OnRead), null);
         }
 
         private void OnRead(IAsyncResult ar)
         {
-            int receivedBytes = stream.EndRead(ar);
-            this.totalBuffer += Encoding.ASCII.GetString(buffer, 0, receivedBytes);
-            string eof = $"<{TagErgo.EOF.ToString()}>";
+            int count = this.stream.EndRead(ar);
+			this.totalBuffer += Encrypter.Decrypt(this.buffer.SubArray(0, count), "password123");
 
-            while (totalBuffer.Contains(eof))
+            string eof = $"<{Tag.EOF.ToString()}>";
+            while (this.totalBuffer.Contains(eof))
             {
-                string packet = totalBuffer.Substring(0, totalBuffer.IndexOf(eof) + 5);
-                totalBuffer = totalBuffer.Substring(packet.IndexOf(eof) + 5);
+                string packet = this.totalBuffer.Substring(0, this.totalBuffer.IndexOf(eof) + eof.Length);
+                this.totalBuffer = this.totalBuffer.Substring(packet.IndexOf(eof) + eof.Length);
 
-                HandlePacket(packet);
+                this.HandlePacket(packet);
             }
-            this.stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+            this.stream.BeginRead(this.buffer, 0, this.buffer.Length, new AsyncCallback(OnRead), null);
         }
 
 		public void Write(string message)
 		{
-			this.stream.Write(Encoding.ASCII.GetBytes(message), 0, message.Length);
+			byte[] encrypted = Encrypter.Encrypt(message, "password123");
+			this.stream.Write(encrypted, 0, encrypted.Length);
 			this.stream.Flush();
 		}
 
-        private void HandlePacket(string packet)
-        {
-            // test purposes, ignore the following:
-            //string etValue = GetValueByTag(TagErgo.ET, packet);
-            //string dtValue = GetValueByTag(TagErgo.DT, packet);
-            //string spValue = GetValueByTag(TagErgo.SP, packet);
-            //string hrValue = GetValueByTag(TagErgo.HR, packet);
-            //string ecValue = GetValueByTag(TagErgo.EC, packet);
-            //string icValue = GetValueByTag(TagErgo.IC, packet);
-            //string apValue = GetValueByTag(TagErgo.AP, packet);
-            //string ipValue = GetValueByTag(TagErgo.IP, packet);
-            //string idValue = GetValueByTag(TagErgo.ID, packet);
-            //string tsValue = GetValueByTag(TagErgo.TS, packet);
+		private void HandlePacket(string packet)
+		{
+			//Console.WriteLine(packet); return;
 
-            string mtValue = GetValueByTag(TagErgo.MT, packet);
+			// test purposes, ignore the following:
+			//string etValue = GetValueByTag(TagErgo.ET, packet);
+			//string dtValue = GetValueByTag(TagErgo.DT, packet);
+			//string spValue = GetValueByTag(TagErgo.SP, packet);
+			//string hrValue = GetValueByTag(TagErgo.HR, packet);
+			//string ecValue = GetValueByTag(TagErgo.EC, packet);
+			//string icValue = GetValueByTag(TagErgo.IC, packet);
+			//string apValue = GetValueByTag(TagErgo.AP, packet);
+			//string ipValue = GetValueByTag(TagErgo.IP, packet);
+			//string idValue = GetValueByTag(TagErgo.ID, packet);
+			//string tsValue = GetValueByTag(TagErgo.TS, packet);
+
+			string mtValue = TagDecoder.GetValueByTag(Tag.MT, packet);
 
 			if (mtValue == "doctor")
 			{
 				this.HandleInputDoctor(packet);
 			}
+			else if (mtValue == "ergo")
+			{
+				this.HandleInputErgo(packet);
+			}
 
-            // Fastest way to handle the data.
-            foreach (TagErgo tag in (TagErgo[])Enum.GetValues(typeof(TagErgo)))
-            {
-                switch (mtValue)
-                {
+			// Fastest way to handle the data.
+			foreach (Tag tag in (Tag[])Enum.GetValues(typeof(Tag)))
+			{
+				switch (mtValue)
+				{
 					case "data":
-						string idValue = GetValueByTag(TagErgo.ID, packet);
-						string tsValue = GetValueByTag(TagErgo.TS, packet);
-						this.HandleInputErgo(tag, GetValueByTag(tag, packet), idValue, tsValue);
+						string idValue = TagDecoder.GetValueByTag(Tag.ID, packet);
+						string tsValue = TagDecoder.GetValueByTag(Tag.TS, packet);
+						string pnuValue = TagDecoder.GetValueByTag(Tag.PNU, packet);
+						this.HandleInputErgo(tag, TagDecoder.GetValueByTag(tag, packet), idValue, tsValue, pnuValue);
 						break;
-                    // Default case should be changed to a real case x:, Will the value of mt also be an enum? >> If not should be a string / integer.
-                }
-            }
-        }
+					// Default case should be changed to a real case x:, Will the value of mt also be an enum? >> If not should be a string / integer.
+				}
+			}
+		}
 
-        private void HandleInputErgo(TagErgo tag, string value, string ergoID, string timestamp)
+		private void HandleInputErgo(string packet)
+		{
+			string action = TagDecoder.GetValueByTag(Tag.AC, packet);
+			if (action == "setid")
+			{
+				this.HandleSetErgoID(packet);
+			}
+		}
+
+		private void HandleSetErgoID(string packet)
+		{
+			this.ergoID = TagDecoder.GetValueByTag(Tag.ID, packet);
+		}
+
+		private void HandleInputErgo(Tag tag, string value, string ergoID, string timestamp, string pnu)
         {
             if (value != null)
             {
                 ClientData clientData;
-                if (!this.server.clientDatas.TryGetValue(ergoID, out clientData))
+                if (!clientDatas.TryGetValue(ergoID, out clientData))
                 {
                     clientData = new ClientData();
-					this.server.clientDatas.Add(ergoID, clientData);
+                    clientDatas.Add(ergoID, clientData);
+                    //if (pnu != null)
+                    //    boundData.Add(pnu, ergoID);
                 }
                 switch (tag) // Timestamp should also be injected below! Adding it seperate is basically useless.
                 {
-                    case TagErgo.ET:
+                    case Tag.ET:
                         clientData.AddET(value, timestamp);
                         break;
-                    case TagErgo.DT:
+                    case Tag.DT:
                         clientData.AddDT(value, timestamp);
                         break;
-                    case TagErgo.SP:
+                    case Tag.SP:
                         clientData.AddSP(value, timestamp);
                         break;
-                    case TagErgo.HR:
+                    case Tag.HR:
                         clientData.AddHR(value, timestamp);
                         break;
-                    case TagErgo.EC:
+                    case Tag.EC:
                         clientData.AddEC(value, timestamp);
                         break;
-                    case TagErgo.IC:
+                    case Tag.IC:
                         clientData.AddIC(value, timestamp);
                         break;
-                    case TagErgo.AP:
+                    case Tag.AP:
                         clientData.AddAP(value, timestamp);
                         break;
-                    case TagErgo.IP:
+                    case Tag.IP:
                         clientData.AddIP(value, timestamp);
+                        break;
+                    case Tag.PNA:
+                        clientData.SetPNA(value);
+                        break;
+                    case Tag.PNU:
+                        clientData.SetPNU(value);
                         break;
                 }
             }
@@ -163,27 +165,56 @@ namespace Server
 
         private void HandleInputDoctor(string packet)
         {
-			string action = GetValueByTag(TagErgo.AC, packet);
+			string action = TagDecoder.GetValueByTag(Tag.AC, packet);
 
 			if (action == "login")
 			{
 				this.HandleDoctorLogin(packet);
 			}
-			else if (action == "brake")
+			else if (action == "emergencybrake")
 			{
 				this.HandleEmergencyBrake(packet);
+			}
+			else if (action == "brake")
+			{
+				this.HandleSessionStop(packet);
 			}
 			else if (action == "resistance")
 			{
 				this.HandleSetResistance(packet);
+			}
+			else if (action == "message")
+			{
+				this.HandleDoctorMessage(packet);
+			}
+		}
+
+		private void HandleSessionStop(string packet)
+		{
+			string ergoID = TagDecoder.GetValueByTag(Tag.ID, packet);
+			this.server.WriteToSpecificErgo(ergoID, $"<{Tag.MT.ToString()}>ergo<{Tag.AC.ToString()}>brake<{Tag.EOF.ToString()}>");
+		}
+
+		private void HandleDoctorMessage(string packet)
+		{
+			string id = TagDecoder.GetValueByTag(Tag.ID, packet);
+			string message = TagDecoder.GetValueByTag(Tag.DM, packet);
+			if (id == "all")
+			{
+				this.server.BroadcastDoctorsMessage($"<{Tag.MT.ToString()}>ergo<{Tag.AC.ToString()}>message<{Tag.DM.ToString()}>{message}<{Tag.EOF.ToString()}>");
+			}
+			else
+			{
+				this.server.WriteToSpecificErgo(id, $"<{Tag.MT.ToString()}>ergo<{Tag.AC.ToString()}>message<{Tag.DM.ToString()}>{message}<{Tag.EOF.ToString()}>");
 			}
 		}
 
 		private void HandleDoctorLogin(string packet)
 		{
 			// TODO: Check if doctors password is valid
+			string username = TagDecoder.GetValueByTag(Tag.UN, packet);
+			string password = TagDecoder.GetValueByTag(Tag.PW, packet);
 
-			Console.WriteLine(packet);
 			this.server.doctor = this;
 			this.server.streaming = true;
 
@@ -192,65 +223,21 @@ namespace Server
 
 		private void HandleEmergencyBrake(string packet)
 		{
-			Console.WriteLine(packet);
-			string bikeID = GetValueByTag(TagErgo.ID, packet);
-
-			// Packet:
-			// <MT>doctor<AC>brake<ID>00472<EOF>
+			string bikeID = TagDecoder.GetValueByTag(Tag.ID, packet);
+			this.server.WriteToSpecificErgo(bikeID, $"<{Tag.MT.ToString()}>ergo<{Tag.AC.ToString()}>emergencybrake<{Tag.EOF.ToString()}>");
 		}
 
 		private void HandleSetResistance(string packet)
 		{
-			Console.WriteLine(packet);
-			throw new NotImplementedException();
+			string bikeID = TagDecoder.GetValueByTag(Tag.ID, packet);
+			string resistance = TagDecoder.GetValueByTag(Tag.SR, packet);
+
+			this.server.WriteToSpecificErgo(bikeID, $"<{Tag.MT.ToString()}>ergo<{Tag.AC.ToString()}>resistance<{Tag.SR}>{resistance}<{Tag.EOF.ToString()}>");
 		}
 
-        private void HandleInputVR()
+		private void HandleInputVR()
         {
             throw new NotImplementedException();
-        }
-
-        public static string GetValueByTag(TagErgo tag, string packet)
-        {
-            char openTag = '<';
-            char closeTag = '>';
-            if (tag != TagErgo.EOF)
-            {
-                string completeTag = $"{openTag}{tag.ToString()}{closeTag}";
-                if (packet.Contains(completeTag))
-                {
-                  //  Console.WriteLine("Found and processed tag! {tag.ToString()}");
-                    int startPosition = -1;
-                    int endPosition = -1;
-                    for (int i = packet.IndexOf(completeTag); i < packet.Length; i++)
-                    {
-                        char characterAtIndex = packet[i];
-                        if (characterAtIndex == closeTag && i + 1 < packet.Length)
-                        {
-                            startPosition = i + 1;
-                            break;
-                        }
-                    }
-                    for (int i = startPosition; i < packet.Length; i++)
-                    {
-                        char characterAtIndex = packet[i];
-                        if (characterAtIndex == openTag && i - 1 >= 0)
-                        {
-                            endPosition = i;
-                            break;
-                        }
-                    }
-                    try
-                    {
-                        string value = packet.Substring(startPosition, endPosition - startPosition);
-                        //Console.WriteLine($"Found value corresponding with tag : {completeTag}{value}");
-                        return value;
-                    }
-                    catch (ArgumentOutOfRangeException e) { Console.WriteLine($"Apparently something went wrong in the GetValueByTag() method located in the ServerClient class. Have you changed any code? {e.ToString()}"); }
-                }
-               // else Console.WriteLine("String does not contain your searched tag, have you added tags? Search tag: " + tag.ToString());
-            }
-			return null;
         }
     }
 }
