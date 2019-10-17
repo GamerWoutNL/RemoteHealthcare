05e1cf3d26b8cfae303e87e4451573c9ba9773c5
Luke Taylor <luketaylor001@gmail.com>
2019-10-17 14:47:36 +0200

    Big oopsie

--- Server/Data/FileWriter.cs
+++ Server/Data/FileWriter.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Server.Data
 {
-    class Class1
+    class FileWriter
     {
+        public void writeFile(string path, string message)
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+
+        }
+
+        public bool checkPassword(string us, string pw)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\RemoteHealthcare\DoctorLogin.txt";
+            string s;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                s = sr.ReadLine();
+                if (TagDecoder.GetValueByTag(Tag.UN, s) == us)
+                {
+                    if (TagDecoder.GetValueByTag(Tag.PW, s) == pw)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return false;
+
+            }
+
+        }
     }
 }
